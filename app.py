746be2f9276686d22d7d9dbae92624be1ee69ee7
OloaneShark <oloanedennis0@gmail.com>
2026-06-27 18:06:22 -0400

    Add user registration

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import LoginManager
-
+from forms import RegistrationForm
 from models import db, User
+import bcrypt
 
 app = Flask(__name__)
 
@@ -24,6 +25,31 @@ def load_user(user_id):
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        hashed_password = bcrypt.hashpw(
+            form.password.data.encode("utf-8"),
+            bcrypt.gensalt()
+        ).decode("utf-8")
+        
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        flash("Account successfully created! You can now log in.", "success")
+        return redirect(url_for("home"))
+    
+    return render_template("register.html", form=form)
 
 
 with app.app_context():
