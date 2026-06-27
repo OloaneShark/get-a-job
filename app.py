0cb84bb9b0c274c52d9ba6e4352d25abf568d78b
OloaneShark <oloanedennis0@gmail.com>
2026-06-27 01:34:24 -0400

    Set up Flask app, database, and registration form

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 
 from flask import Flask, render_template
+from flask_login import LoginManager
+
+from models import db, User
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "super-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
